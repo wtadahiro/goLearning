4e92bcb03c827d74411d91b280014c48ac30b397
Tadahiro Wada <w.tadahiro@gmail.com>
2015-06-05 21:36:22 +0700

    add another way of bucketSort

--- src/bucketSort/bucketSort.go
+++ src/bucketSort/bucketSort.go
@@ -9,10 +9,13 @@ import(
 func main(){
 	inSlice := generateRandomSlice(100)
 	sortedSlice := bucketSort(inSlice)
+	sortedSlice2 := bucketSort2(inSlice)
 	fmt.Println("-------beforeSort-------")
 	fmt.Println(inSlice)
 	fmt.Println("-------afterSort-------")
-	fmt.Print(sortedSlice)
+	fmt.Println(sortedSlice)
+	fmt.Println("-------afterSort2-------")
+	fmt.Println(sortedSlice2)
 
 }
 
@@ -29,6 +32,23 @@ func bucketSort(inSlice []int) []int{
 			outSlice = append(outSlice, bucket[i][k])
 		}
 	}
+	return outSlice
+}
+
+func bucketSort2(inSlice []int) []int{
+	var outSlice []int
+
+	bucket := make([]int, 100)
+	for i:=0; i<len(inSlice); i++{
+		bucket[inSlice[i]] ++
+	}
+
+	for i:=0; i<100; i++{
+		for k:=0; k<bucket[i]; k++{
+			outSlice = append(outSlice, i)
+		}
+	}
+
 	return outSlice
 }
 
